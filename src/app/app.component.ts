22f3e70596e91de545cbddc88f683f003b189f17
iesous-kurios <j.wylie.81@gmail.com>
2022-02-10 15:12:43 -0800

    Current progress on learn angular course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+
+  onButtonClick() {
+    this.password = 'MY PASSWORD!!!';
+    console.log("State of include letters" + " " + this.includeLetters);
+    console.log("State of include numbers" + " " + this.includeNumbers);
+    console.log("State of include symbols" + " " + this.includeSymbols);
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
 }
